ec6eec14b5d58510dd838fc5aacd084905c9f100
eray <alieraykt@gmail.com>
2025-11-04 22:41:22 +0300

    Replace ImGui window with SFML shape example
    Simplified main.cpp to display a green circle using SFML, removing ImGui and ImGui-SFML integration. Updated project filters to group ImGui-related source files under a new 'ImGui Files' filter for better organization.

--- main.cpp
+++ main.cpp
@@ -16,36 +16,23 @@ extern "C" {
 
 int main()
 {
-	sf::Event event;
-	sf::Clock clock;
-	sf::Time deltaTime;
-	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Window", sf::Style::Default);
-	ImGui::SFML::Init(window);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-	while (window.isOpen())
-	{
-		deltaTime = clock.restart();
-		while (window.pollEvent(event))
-		{
-			ImGui::SFML::ProcessEvent(window, event);
-			if (event.type = sf::Event::Closed)
-			{
-				window.close();
-			}
-		}
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-		ImGui::SFML::Update(window, deltaTime);
-		window.clear();
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
-		if (ImGui::Begin("Example Window"))
-		{
-			ImGui::Text("Hello");
-			ImGui::End();
-		}
-
-		ImGui::SFML::Render(window);
-		window.display();
-	}
-	return 0;
-
+    return 0;
 }
